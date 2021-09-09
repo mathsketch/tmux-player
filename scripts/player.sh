b64146ed45185183e0990bb656324b13f00c7705
arux <ktonux@outlook.com>
2021-09-09 17:10:23 +0800

    tmux player plugin

---
+++ scripts/player.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$CURRENT_DIR/helpers.sh"
+
+main() {
+    local player_style_left=$(get_tmux_option "@player-style-left")
+    local player_style_right=$(get_tmux_option "@player-style-right")
+    local player_format=$(get_tmux_option "@player-format")
+
+    if [ -f "$(which playerctl)" ];then
+        local value=$(playerctl metadata -f "$player_format" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+    fi
+
+    if [ -n "$value" ];then
+        echo -n "${player_style_left}${value}${player_style_right}"
+    fi
+}
+
+main
