0e6ff22b0f7f3d1e78ce9a66fd5331c6fe3beb49
arux <ktonux@outlook.com>
2022-02-08 20:52:42 +0800

    add max-length option

--- scripts/player.sh
+++ scripts/player.sh
@@ -7,9 +7,10 @@ main() {
     local player_style_left=$(get_tmux_option "@player-style-left")
     local player_style_right=$(get_tmux_option "@player-style-right")
     local player_format=$(get_tmux_option "@player-format")
+    local max_length=$(get_tmux_option "@player-max-length")
 
     if [ -f "$(which playerctl)" ];then
-        local value=$(playerctl metadata -f "$player_format" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+        local value=$(playerctl metadata -f "$player_format" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | sed -re "s/(.{$max_length}).*/\1.../")
     fi
 
     if [ -n "$value" ];then
